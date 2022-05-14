e6d04f18ad6d26657b9e8aa36ace41b5c6dfe5d7
minchoi <53928609+minchoi9509@users.noreply.github.com>
2022-05-14 10:27:06 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,11 +9,23 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    /*
+     * 역할과 구현 클래스가 한눈에 들어온다. 애플리케이션의 전체 구성이 어떻게 되어 있는지 파악이 가능하다.
+     */
+
     public MemberSerivce memberSerivce() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
