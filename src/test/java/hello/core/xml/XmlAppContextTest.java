12e28099517ef24409fb6fd32b7ac52cc0169ebf
minchoi <53928609+minchoi9509@users.noreply.github.com>
2022-05-21 23:55:34 +0900

    다양한 설정 형식 지원 - 자바 코드, XML

---
+++ src/test/java/hello/core/xml/XmlAppContextTest.java
@@ -0,0 +1,18 @@
+package hello.core.xml;
+
+import hello.core.member.MemberSerivce;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContextTest {
+    @Test
+    void xmlAppContext() {
+
+        // appConfig.xml 파일을 생성해서 구현하면 똑같음.
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberSerivce memberService = ac.getBean("memberService", MemberSerivce.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberSerivce.class);
+    }
+}
